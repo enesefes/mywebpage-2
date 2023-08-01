4f6f75ab6a354470b1c74d9b9d681a78c2dc7f31
Enes EFE <40739760+enesefes@users.noreply.github.com>
2023-08-01 22:54:35 +0300

    Add files via upload

---
+++ precache-manifest.a2bb42e19c7c11e1501faceb03d53187.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ed75c774ee2654adee564f9cb2e3ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ee33075378e3c3c9a96",
+    "url": "/static/css/main.b9b8048e.chunk.css"
+  },
+  {
+    "revision": "e936baa8efd9cc53f3e4",
+    "url": "/static/js/2.6dd2552f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6dd2552f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee33075378e3c3c9a96",
+    "url": "/static/js/main.262cee82.chunk.js"
+  },
+  {
+    "revision": "976e9d77ee5dd28c4220",
+    "url": "/static/js/runtime-main.147274ae.js"
+  },
+  {
+    "revision": "963bd212ed66b65cb48ebb99a22377df",
+    "url": "/static/media/Logo.963bd212.png"
+  },
+  {
+    "revision": "ca3e33ae50a0fd8d34102a61b24379e5",
+    "url": "/static/media/code.ca3e33ae.svg"
+  },
+  {
+    "revision": "82fed24c47181b908d182889c34516b1",
+    "url": "/static/media/logo-transparent.82fed24c.png"
+  },
+  {
+    "revision": "0c0d602239f137a10810d23c6603ad27",
+    "url": "/static/media/myphoto.0c0d6022.jpg"
+  }
+]);
